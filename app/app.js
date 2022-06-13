0bf08ff57b6053e9d95f06f971d986b1426709bc
ryan-spearman <ryanspearman@gmail.com>
2022-06-13 18:12:41 -0500

    Added Starter Code  Linked CSS and JS files  Added Test Code

---
+++ app/app.js
@@ -0,0 +1,73 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const ele = document.getElementsByClassName('landing__container')
+console.log(ele)
+
+
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+for (let i =0; i < ele.length; i++) {
+    console.log(ele[i])
+    ele[i].addEventListener('click', function() {
+        ele[i].style.backgroundColor="pink"
+    })
+}
+
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
